d3170754d99a0bf2d8e61fca90b38d5b8b6d7c9f
Jonas <jonasmachado11@gmail.com>
2024-03-28 16:19:17 -0300

    Adicionando voice

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female',{rate:1.2});
 }
 
 function exibirMensagemInicial() {
